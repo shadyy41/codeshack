3dd9fcf9079988a0bc9aaea1eaec17f0c0e324b7
shady41 <abhinavjha2512@gmail.com>
2023-06-05 16:09:22 +0530

    Added mongodb database

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import type { NextAuthOptions } from 'next-auth'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "@/app/lib/mongodb"
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -12,7 +14,9 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: '/auth/signin',
     signOut: '/auth/signout'
-  }
+  },
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET
 }
 
 const handler = NextAuth(
