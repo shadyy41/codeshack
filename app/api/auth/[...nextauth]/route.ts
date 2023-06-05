f0c976c75316f0a686062ed8c10f990fea981ac7
shady41 <abhinavjha2512@gmail.com>
2023-06-05 15:34:44 +0530

    Added google oauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,22 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import type { NextAuthOptions } from 'next-auth'
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
   ],
-})
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout'
+  }
+}
+
+const handler = NextAuth(
+  authOptions
+)
 
 export { handler as GET, handler as POST }
