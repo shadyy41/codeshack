15850892ca60d925d732d0ed19b00dfcfbdfe9ff
shady41 <abhinavjha2512@gmail.com>
2023-06-13 19:54:52 +0530

    Changed buttons

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link"
 import { authOptions } from "./api/auth/[...nextauth]/route"
 import { getServerSession } from "next-auth"
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline"
-import RoomForm from "./components/roomform"
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -25,7 +24,13 @@ export default async function Home() {
         </div>
         <p className="text-slate-400 text-2xl max-w-sm w-full break-words">Peer to peer video calls, collaborative coding, and much more!</p>
         <br />
-        <RoomForm authenticated={ session!==null } name={ name } userImage = { userImage } isPremium = { isPremium }/>
+        <div className="flex flex-col w-full text-md gap-3 text-slate-300 max-w-xs">
+          {/* <input type="text" placeholder="Enter your name" className="w-full py-3 px-4 rounded-md border-2 border-white border-opacity-20 hover:border-opacity-40 transition-colors custom-outline bg-transparent backdrop-blur-sm"/> */}
+          <Link className="w-full py-3 rounded-md bg-blue-600 text-white hover:bg-blue-500 transition-colors custom-outline" href="/room/create">Create Room</Link>
+          <a className="py-3 rounded-md border border-white border-opacity-20 text-white hover:bg-neutral-900 bg-neutral-950 transition-colors custom-outline w-full max-w-sm" href="https://github.com/shadyy41/codeshack" target="_blank" rel="noopener noreferrer">
+            Visit Repo
+          </a>
+        </div>
       </div>
       <div className="border border-white border-opacity-10 rounded-lg overflow-hidden flex-grow aspect-[16/10] hidden lg:flex backdrop-blur-md relative">
         <Image src={banner} alt="banner image" placeholder="blur" fill={true}/>
