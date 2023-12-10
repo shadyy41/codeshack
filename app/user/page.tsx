a32330d7b290e94ec85c1947a4091babbbecdfcd
shady41 <abhinavjha2512@gmail.com>
2023-12-11 00:52:24 +0530

    Added divider

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation"
 import FormHeader from "../components/formheader"
 import UserimageForm from "./components/userimageform"
 import Link from "next/link"
+import Divider from "../components/divider"
 
 export const metadata = {
   title: 'Edit Profile',
@@ -25,6 +26,7 @@ const User = async () => {
         Join <Link href="/premium" className="premium-text font-bold">Premium</Link> to enable profile customization!
         </div>
       }
+      <Divider/>
       <FormHeader text="Account Information"/>
       <UsernameForm name={ name } isPremium = { isPremium } email={ email }/>
       <FormHeader text="Edit Avatar"/>
